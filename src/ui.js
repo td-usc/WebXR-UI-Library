65dc0799420d9418dc8150154bebdef09051baef
td-usc <tdurbin@usc.edu>
2021-11-11 11:35:16 -0800

    Added header customization and increased modularity
    Added ability to change header appearance and moved more functionality out of constructor into functions.

--- src/ui.js
+++ src/ui.js
@@ -17,10 +17,18 @@ class TextBox {
             size: { width: 1, height: 1},
             backgroundColor: '#909090',
             opacity: 1,
-            font: {family: 'Arial', size: 50, color: '#000000'},
-            lineSpacing: 0.5, // 0 is no spacing, 1 is 1 fontHeight of space
-            padding: {top: 20, bottom: 20, left: 20, right: 20},
-            header: {textColor: '#000000', barColor: '#505050'},
+
+            header: {
+                font: {family: 'Arial', size: 50, color: '#000000'}, // Font will be bold
+                barColor: '#505050', 
+                padding: { top: 10, bottom: 10, left: 10, right: 10 } 
+            },
+
+            content: {
+                font: {family: 'Arial', size: 50, color: '#000000'}, // Font will be regular
+                lineSpacing: 0.5, // 0 is no spacing, 1 is 1 fontHeight of space
+                padding: {top: 20, bottom: 20, left: 20, right: 20},
+            },
         }
 
         // Read in Header
@@ -31,81 +39,85 @@ class TextBox {
 
        // Read in Config
         if (config !== undefined) {
-          if (config.position !== undefined) { 
-              if (config.position.x !== undefined) this.config.position.x = config.position.x;
-              if (config.position.y !== undefined) this.config.position.y = config.position.y;
-              if (config.position.y !== undefined) this.config.position.y = config.position.y;
-          }
+            if (config.position !== undefined) { 
+                if (config.position.x !== undefined) this.config.position.x = config.position.x;
+                if (config.position.y !== undefined) this.config.position.y = config.position.y;
+                if (config.position.y !== undefined) this.config.position.y = config.position.y;
+            }
 
-          if (config.rotation !== undefined) { 
-            if (config.rotation.x !== undefined) this.config.rotation.x = config.rotation.x;
-            if (config.rotation.y !== undefined) this.config.rotation.y = config.rotation.y;
-            if (config.rotation.y !== undefined) this.config.rotation.y = config.rotation.y;
-        }
+            if (config.rotation !== undefined) { 
+                if (config.rotation.x !== undefined) this.config.rotation.x = config.rotation.x;
+                if (config.rotation.y !== undefined) this.config.rotation.y = config.rotation.y;
+                if (config.rotation.y !== undefined) this.config.rotation.y = config.rotation.y;
+            }
           
-          if (config.size !== undefined) { 
-              if (config.size.width !== undefined) this.config.size.width = config.size.width;
-              if (config.size.height !== undefined) this.config.size.height = config.size.height;
-          }
+            if (config.size !== undefined) { 
+                if (config.size.width !== undefined) this.config.size.width = config.size.width;
+                if (config.size.height !== undefined) this.config.size.height = config.size.height;
+            }
 
-          if (config.backgroundColor !== undefined) this.config.backgroundColor = config.backgroundColor;
-          if (config.opactiy !== undefined) this.config.opacity = config.opacity;
+            if (config.backgroundColor !== undefined) this.config.backgroundColor = config.backgroundColor;
 
-          if (config.font !== undefined) { 
-              if (config.font.family !== undefined) this.config.font.family = config.font.family;
-              if (config.font.size !== undefined) this.config.font.size = config.font.size;
-              if (config.font.color !== undefined) this.config.font.color = config.font.color;
-          }
+            if (config.opactiy !== undefined) this.config.opacity = config.opacity;
 
-          if (config.lineSpacing !== undefined) this.config.lineSpacing = config.lineSpacing;
+            if (config.header !== undefined) {
+                if (config.header.font !== undefined) { 
+                    if (config.header.font.family !== undefined) this.config.header.font.family = config.header.font.family;
+                    if (config.header.font.size !== undefined) this.config.header.font.size = config.header.font.size;
+                    if (config.header.font.color !== undefined) this.config.header.font.color = config.header.font.color;
+                }
+                if (config.header.barColor !== undefined) this.config.header.barColor = config.header.barColor;
+                if (config.header.padding !== undefined) {
+                    if (config.header.padding.left !== undefined) this.config.header.padding.left = config.header.padding.left;
+                    if (config.header.padding.right !== undefined) this.config.header.padding.right = config.header.padding.right;
+                    if (config.header.padding.top !== undefined) this.config.header.padding.top = config.header.padding.top;
+                    if (config.header.padding.bottom !== undefined) this.config.header.padding.bottom = config.header.padding.bottom;
+                }
+            }
 
-          if (config.padding !== undefined) { 
-              if (config.padding.left !== undefined) this.config.padding.left = config.padding.left;
-              if (config.padding.right !== undefined) this.config.padding.right = config.padding.right;
-              if (config.padding.top !== undefined) this.config.padding.top = config.padding.top;
-              if (config.padding.bottom !== undefined) this.config.padding.bottom = config.padding.bottom;
-          }
+            if (config.content !== undefined) {
 
-          if (config.header !== undefined) {
-            if (config.header.text !== undefined) this.config.header.text = config.header.text;
-            if (config.header.textColor !== undefined) this.config.header.textColor = config.header.textColor;
-            if (config.header.barColor !== undefined) this.config.header.barColor = config.header.barColor;
-          }
-       }
+                if (config.content.font !== undefined) { 
+                    if (config.content.font.family !== undefined) this.config.content.font.family = config.content.font.family;
+                    if (config.content.font.size !== undefined) this.config.content.font.size = config.content.font.size;
+                    if (config.content.font.color !== undefined) this.config.content.font.color = config.content.font.color;
+                }
+
+                if (config.content.lineSpacing !== undefined) this.config.content.lineSpacing = config.content.lineSpacing;
+
+                if (config.content.padding !== undefined) { 
+                    if (config.content.padding.left !== undefined) this.config.content.padding.left = config.content.padding.left;
+                    if (config.content.padding.right !== undefined) this.config.content.padding.right = config.content.padding.right;
+                    if (config.content.padding.top !== undefined) this.config.content.padding.top = config.content.padding.top;
+                    if (config.content.padding.bottom !== undefined) this.config.content.padding.bottom = config.content.padding.bottom;
+                }
+            }
+        }
 
         // Create Canvas
         const canvas = createCanvas(this.config.size.width, this.config.size.height);
-        this.context = canvas.getContext('2d');
-
-        // Set Canvas Properties
-        this.context.textAlign = 'start';
-        this.context. textBaseline = 'top';
-        this.context.fillStyle = this.config.backgroundColor;
-        this.context.fillRect(0, 0, canvas.width, canvas.height);
+        
+        //Set Canvas Properties -- Text alignment is Top Start
+        this.context = setCanvasContext(canvas, this.config.backgroundColor);
 
         // Header
-        placeHeader(canvas, this.context, this.header, this.config.header, this.config.font, this.config.padding);
-
-        // Set Font for Body
-        this.context.font = this.config.font.size + 'px ' + this.config.font.family;
-        this.context.fillStyle = this.config.font.color; 
+        let headerHeight = placeHeader(canvas, this.context, this.header, this.config.header);
 
        // Determine Word Wrap and Place Text
         let boundingBox = {
-            top: this.context.measureText('M').width + this.config.padding.top, 
-            bottom: canvas.height - this.config.padding.bottom,
-            left: this.config.padding.left,
-            right: canvas.width - this.config.padding.right 
+            top: headerHeight + this.config.content.padding.top, 
+            bottom: canvas.height - this.config.content.padding.bottom,
+            left: this.config.content.padding.left,
+            right: canvas.width - this.config.content.padding.right 
         }
 
-        this.lines = wordWrap(boundingBox, this.content, this.context);
-        placeText(canvas, this.context, this.lines, boundingBox, this.config.lineSpacing);
+        placeText(canvas, this.context, boundingBox, this.content, this.config.content);
 
         this.context.save();
 
         this.texture = new THREE.CanvasTexture(canvas);
 
-        // Create Plane to Put Texture Onto
+        // Create Plane and Apply Texture
         this.mesh = createPlane(this.config.position, this.config.rotation, this.config.size, this.config.opacity, this.texture);
     }
 
@@ -120,15 +132,27 @@ function createCanvas(width, height) {
     return canvas;
 }
 
+function setCanvasContext(canvas, backgroundColor) {
+    let context = canvas.getContext('2d');
+    context.textAlign = 'start';
+    context. textBaseline = 'top';
+    context.fillStyle = backgroundColor;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    return context
+}
 
-function placeHeader(canvas, context, header, headerConfig, font, padding) {
-    context.font = 'Bold ' + font.size + 'px ' + font.family;
+
+function placeHeader(canvas, context, header, headerConfig) {
+    context.font = 'Bold ' + headerConfig.font.size + 'px ' + headerConfig.font.family;
 
     context.fillStyle = headerConfig.barColor;
-    context.fillRect(0, 0, canvas.width, context.measureText('M').width + padding.top); 
+    context.fillRect(0, 0, canvas.width, context.measureText('M').width + headerConfig.padding.top + headerConfig.padding.bottom); 
 
-    context.fillStyle = headerConfig.textColor; 
-    context.fillText(header, padding.left / 2, padding.top / 2);
+    context.fillStyle = headerConfig.font.color; 
+    context.fillText(header, headerConfig.padding.left, headerConfig.padding.top, canvas.width - headerConfig.padding.left - headerConfig.padding.right);
+
+    // Return Height of the Header Box
+    return context.measureText('M').width + headerConfig.padding.top + headerConfig.padding.bottom;
 }
 
 
@@ -153,16 +177,22 @@ function wordWrap(boundingBox, content, context) {
  }
 
 
-function placeText(canvas, context, lineArray, boundingBox, lineSpacing) {
-    for (let iter = 0; iter < lineArray.length; iter++) {
+function placeText(canvas, context, boundingBox, content, contentConfig) {
+
+    context.font = contentConfig.font.size + 'px ' + contentConfig.font.family;
+    context.fillStyle = contentConfig.font.color;
+
+    let lines = wordWrap(boundingBox, content, context);
+
+    for (let iter = 0; iter < lines.length; iter++) {
 
         let fontHeight = context.measureText('M').width;
 
-        // Offset Lines from Top: top of bounding box + height of previous lines + line spacing
-        let lineHeight = boundingBox.top + iter * fontHeight + ((iter + 1) * fontHeight * lineSpacing);
+        // Offset Lines from Top: top of bounding box + height of previous lines + previous line spacing
+        let lineHeight = boundingBox.top + iter * fontHeight + (iter * fontHeight * contentConfig.lineSpacing);
 
         if (lineHeight < boundingBox.bottom - fontHeight)
-            context.fillText(lineArray[iter], boundingBox.left, lineHeight);
+            context.fillText(lines[iter], boundingBox.left, lineHeight);
 
     }
 }
